22fb8ad1dda352779a3e1c3c7ab2c55d2ea8b019
Ichinga Samuel <46368656+Ichinga-Samuel@users.noreply.github.com>
2023-07-21 15:06:23 +0100

    Update main.py

--- fastfiles/main.py
+++ fastfiles/main.py
@@ -8,53 +8,47 @@ class FileData(BaseModel):
     """
     Represents the result of an upload operation
     Attributes:
-        public_url (HttpUrl | str): A public url for accessing the object
-        status (bool): True or False if upload is successful
+        public_url (HttpUrl | str): A public URL for accessing the object
+        status (bool): True if the upload is successful else False.
     """
     public_url: HttpUrl | str = ""
-    status: bool = True
+    status: bool = False
 
 
 class CloudUpload(ABC):
     """
-    Keyword Args:
-        file (bytes | UploadFile): File object as bytes or UploadFile object the default value is None
-        files (list[bytes | UploadFile]): A list of files for multiple upload
-        name (str): A name for the file in the cloud object
-        extra_args (dict): A dictionary of optional arguments
-
     Methods:
-        upload: Uploads single object to the cloud
+        upload: Uploads a single object to the cloud
         multi_upload: Upload multiple objects to the cloud
 
     Attributes:
-        response (FileData | list[FileDate]): Response of the file upload operation. A single object for single upload and
-        a list for multiple files upload
+        result (FileData | list[FileData]): Result of the file upload operation.
+        A single FileData object for a single upload and a list of FileData objects for multiple uploads.
     """
 
-    def __init__(self, *, file: bytes | UploadFile | None = None, files: list[UploadFile | bytes] | None = None, name: str = "",
-                 extra_args: dict | None = None):
-
-        self.file = file
-        self.files = files
-        self.response: FileData | list[FileData] = FileData()
-        self.extra_args = extra_args or {}
-        self.name = name
-
-    async def __call__(self, file: bytes | UploadFile | None = None, files: list[UploadFile | bytes] | None = None):
-        if file:
-            self.file = file
-            await self.upload(name=self.name)
-
-        elif files:
-            self.files = files
-            await self.multi_upload()
-        return self
+    def __init__(self, config: dict | None = None):
+        """"
+        Keyword Args:
+            config (dict): A dictionary of config settings
+        """"
+        self.result: FileData | list[FileData] = FileData()
+        self.config = config or {}
+        
+    async def __call__(self, file: UploadFile | None = None, files: list[UploadFile] | None = None) -> FileData | list[FileData]:
+        try:
+            if file:
+                await self.upload(file)
+    
+            elif files:
+                await self.multi_upload(files=files)
+        except Exception as err:
+            pass
+        return self.result
 
     @abstractmethod
-    async def upload(self, name: str = "") -> FileData:
+    async def upload(self, *, file: UploadFile | None = None) -> FileData:
         """"""
 
     @abstractmethod
-    async def multi_upload(self, *args, **kwargs) -> list[FileData]:
+    async def multi_upload(self, *, files: list[UploadFile]) -> list[FileData]:
         """"""
