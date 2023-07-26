4dd7b731f35932449f4ef7078ae8f934e41a1aa9
Ichinga Samuel <46368656+Ichinga-Samuel@users.noreply.github.com>
2023-07-26 14:10:35 +0100

    Update s3.py

--- fastfiles/s3.py
+++ fastfiles/s3.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 import logging
 from urllib.parse import quote as urlencode
+from functools import cache
 
 import boto3
 from botocore.exceptions import ClientError, NoCredentialsError
@@ -11,42 +12,27 @@ from .main import CloudUpload, FileData, UploadFile
 logger = logging.getLogger(__name__)
 
 
-class S3(CloudUpload):
+class S3(CloudUpload)
+    @property
+    @cache
+    def client(self):
+        key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+        access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+        region_name = os.environ.get('AWS_DEFAULT_REGION') or self.config.get('region')
+        return boto3.client('s3', region_name=region_name, aws_access_key_id=key_id, aws_secret_access_key=access_key)
 
-    def __init__(self, *, file: bytes | UploadFile | None = None, files: list[UploadFile | bytes] | None = None, region_name: str = "",
-                 aws_access_key_id: str = "", aws_secret_access_key: str = "", bucket_name: str = "", name: str = "", extra_args: dict | None = None):
-
-        super().__init__(file=file, files=files, name=name, extra_args=extra_args)
-        self.region_name = region_name or os.environ.get('AWS_REGION')
-        self.aws_access_key_id = aws_access_key_id or os.environ.get('AWS_ACCESS_KEY')
-        self.aws_secret_access_key = aws_secret_access_key or os.environ.get('AWS_SECRET_KEY')
-        self.bucket_name = bucket_name or os.environ['S3_BUCKET_NAME']
-
-    async def get_client(self):
-        return await asyncio.to_thread(boto3.client, 's3', region_name=self.region_name, aws_access_key_id=self.aws_access_key_id,
-                                       aws_secret_access_key=self.aws_secret_access_key)
-
-    async def upload(self, name: str = ""):
-        """"""
-        name = name or self.name
-        file_data = await self._upload_file(file=self.file, name=name)
-        self.response = file_data
-        return self.response
-
-    async def _upload_file(self, *, file, name: str = "", client=None) -> FileData:
+    async def upload(self, *, file: UploadFile) -> FileData:
         try:
-            s3 = client or await self.get_client()
-            object_name = name or file.filename
-            file = file.file
-            await asyncio.to_thread(s3.upload_fileobj, file, self.bucket_name, object_name, ExtraArgs=self.extra_args)
-            url = f"https://{self.bucket_name}.s3.{self.region_name}.amazonaws.com/{urlencode(object_name.encode('utf8'))}"
-            return FileData(public_url=url)
+            extra_args = self.config.get('extra_args', {})
+            bucket = self.config['bucket']
+            region = os.environ.get('AWS_DEFAULT_REGION') or self.config.get('region')
+            await asyncio.to_thread(self.client.upload_fileobj, file.file, bucket, file.filename, ExtraArgs=extra_args)
+            url = f"https://{self.bucket}.s3.{region}.amazonaws.com/{urlencode(file.filename.encode('utf8'))}"
+            return FileData(url=url, message=f'{file.filename} uploaded successfully')
         except (NoCredentialsError, ClientError, Exception) as err:
             logger.error(err)
-            return FileData(status=False)
+            return FileData(status=False, error=str(err), message='File upload was unsuccessful)
 
-    async def multi_upload(self, *args, **kwargs):
-        client = await self.get_client()
-        tasks = [asyncio.create_task(self._upload_file(file=file, client=client)) for file in self.files]
-        self.response = await asyncio.gather(*tasks)
-        return self.response
+    async def multi_upload(self, *, files: UploadFiles):
+        tasks = [asyncio.create_task(self.upload(file=file)) for file in files]
+        return await asyncio.gather(*tasks)
